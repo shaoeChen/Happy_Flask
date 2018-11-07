d5ea8e6654ec0c36a59efb72d89e0b8110dc9f23
Shaoe.chen <shaoe.hcne@gmail.com>
2018-11-07 21:41:31 +0800

    Flask實作_使用者登入功能_01_優化Html版面_設置base基版

---
+++ 使用者登入功能/Flask實作_使用者登入功能_01_優化Html版面_設置base基版/config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    pjdir = os.path.abspath(os.path.dirname(__file__))
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+                 os.path.join(pjdir, 'static\\data\\data_register.sqlite')
+    SECRET_KEY = b'\xb9k\xdf@\x0e\x1f(\xf2\xb0\xd0\xcb?Y\xdcN\x19G\x12e\xa8\x8b\xe5\xccS'
+    MAIL_SERVER = 'smtp.live.com'
+    MAIL_PROT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'Your Mail Count'
+    MAIL_PASSWORD = 'Your Mail Password'
