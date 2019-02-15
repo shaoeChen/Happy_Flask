1d77492a9a8c30ccb5a868b2c8ebf678f1890afa
NintenDDD <a88115470@gmail.com>
2019-02-15 16:34:27 +0800

    Update model.py
    typo

--- 建置一個使用者註冊頁面/Flask實作_建置一個使用者註冊頁面_01_基板/model.py
+++ 建置一個使用者註冊頁面/Flask實作_建置一個使用者註冊頁面_01_基板/model.py
@@ -3,7 +3,7 @@ from flask_register import db
 class UserReister(db.Model):
     """記錄使用者資料的資料表"""
     __tablename__ = 'UserRgeisters'
-    id = db.Column(db.Integer, primark_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
